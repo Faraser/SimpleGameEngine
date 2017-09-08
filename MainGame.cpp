3d045c31f48284e0c696b49eaa04aec8949919df
Sergey Timoshkin <faraser@yandex-team.ru>
2017-09-08 19:05:57 +0300

    Added glew to cmake

--- MainGame.cpp
+++ MainGame.cpp
@@ -1,5 +1,15 @@
 #include "MainGame.h"
 #include "iostream"
+#include "string"
+#include "GL/glew.h"
+
+void fatalError(std::string errorMsg) {
+    std::cout << errorMsg << " SDL error: " << SDL_GetError()  << std::endl;
+    std::cout << "Press any key to continue..." << std::endl;
+    int c;
+    std::cin >> c;
+    SDL_Quit();
+};
 
 MainGame::MainGame() {
     _window = nullptr;
@@ -15,7 +25,7 @@ void MainGame::run() {
 
 void MainGame::initSystems() {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cout << "SDL could not initialize! SDL_Error:" << SDL_GetError() << std::endl;
+        fatalError("SDL could not initialize!") ;
     };
 
     SDL_Window *_window = SDL_CreateWindow(
@@ -28,8 +38,15 @@ void MainGame::initSystems() {
     );
 
     if (_window == nullptr) {
-        std::cout << "Window could not be created! SDL_Error:" << SDL_GetError() << std::endl;
+        fatalError("Window could not be created!") ;
     }
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(_window);
+    if (glContext == nullptr) {
+        fatalError("SDL_GLcontext could not been create");
+    }
+
+    glewInit();
 }
 
 void MainGame::gameLoop() {
@@ -44,6 +61,9 @@ void MainGame::processInput() {
         switch(evnt.type) {
             case SDL_QUIT:
                 _gameState = GameState::EXIT;
+                break;
+            case SDL_MOUSEMOTION:
+                std::cout << evnt.motion.x << std::endl;
                 break;
         }
     };
--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 int main(int argc, char ** argv) {
     MainGame mainGame;
     mainGame.run();
+
+    return 0;
 }
 
