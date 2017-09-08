c92d6bdc2925cc3b048ff6d4e8c5a24a3acdc4d3
Sergey Timoshkin <faraser@yandex-team.ru>
2017-09-08 23:56:30 +0300

    Added glfw to cmake

--- MainGame.cpp
+++ MainGame.cpp
@@ -4,7 +4,7 @@
 #include "GL/glew.h"
 
 void fatalError(std::string errorMsg) {
-    std::cout << errorMsg << " SDL error: " << SDL_GetError()  << std::endl;
+    std::cout << errorMsg << " SDL error: " << SDL_GetError() << std::endl;
     std::cout << "Press any key to continue..." << std::endl;
     int c;
     std::cin >> c;
@@ -25,7 +25,7 @@ void MainGame::run() {
 
 void MainGame::initSystems() {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        fatalError("SDL could not initialize!") ;
+        fatalError("SDL could not initialize!");
     };
 
     SDL_Window *_window = SDL_CreateWindow(
@@ -38,7 +38,7 @@ void MainGame::initSystems() {
     );
 
     if (_window == nullptr) {
-        fatalError("Window could not be created!") ;
+        fatalError("Window could not be created!");
     }
 
     SDL_GLContext glContext = SDL_GL_CreateContext(_window);
@@ -48,8 +48,12 @@ void MainGame::initSystems() {
 
     GLenum error = glewInit();
     if (error != GLEW_OK) {
-       fatalError("Could not initialize glew");
+        fatalError("Could not initialize glew");
     }
+
+//    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 }
 
 void MainGame::gameLoop() {
@@ -61,18 +65,30 @@ void MainGame::gameLoop() {
 
 void MainGame::processInput() {
     SDL_Event evnt;
-    while(SDL_PollEvent(&evnt)) {
-        switch(evnt.type) {
+    while (SDL_PollEvent(&evnt)) {
+        switch (evnt.type) {
             case SDL_QUIT:
                 _gameState = GameState::EXIT;
                 break;
             case SDL_MOUSEMOTION:
-                std::cout << evnt.motion.x << std::endl;
+//                std::cout << evnt.motion.x << std::endl;
                 break;
         }
     };
 }
 
 void MainGame::drawGame() {
-   glClearDepth(1.0);
+    glClearDepth(1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableClientState(GL_COLOR_ARRAY);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(0, 0);
+    glVertex2f(0, 500);
+    glVertex2f(500, 500);
+
+    glEnd();
+
+    SDL_GL_SwapWindow(_window);
 }
--- main.cpp
+++ main.cpp
@@ -6,4 +6,73 @@ int main(int argc, char ** argv) {
 
     return 0;
 }
+//
+//
+//  main.cpp
+//  openGL
+//
+//  Created by Sergey Timoshkin on 31.08.17.
+//  Copyright © 2017 Sergey Timoshkin. All rights reserved.
+//
 
+//#include <iostream>
+//#include <GL/glew.h>
+//#include "GLFW/glfw3.h"
+//
+//void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+//{
+//    // Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true,
+//    // и приложение после этого закроется
+//    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+//        glfwSetWindowShouldClose(window, GL_TRUE);
+//}
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    glfwInit();
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//    //Минорная
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//    //Установка профайла для которого создается контекст
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//    //Выключение возможности изменения размера окна
+//
+//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+//
+//    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+//    if (window == nullptr)
+//    {
+//        std::cout << "Failed to create GLFW window" << std::endl;
+//        glfwTerminate();
+//        return -1;
+//    }
+//    glfwMakeContextCurrent(window);
+//
+//    glewExperimental = GL_TRUE;
+//    if (glewInit() != GLEW_OK)
+//    {
+//        std::cout << "Failed to initialize GLEW" << std::endl;
+//        return -1;
+//    }
+//
+//    int width, height;
+//    glfwGetFramebufferSize(window, &width, &height);
+//
+//    glViewport(0, 0, width, height);
+//
+//    glfwSetKeyCallback(window, key_callback);
+//
+//    while(!glfwWindowShouldClose(window))
+//    {
+//        glfwPollEvents();
+//        glfwSwapBuffers(window);
+//
+//
+//        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+//        glClear(GL_COLOR_BUFFER_BIT);
+//    }
+//    glfwTerminate();
+//
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
