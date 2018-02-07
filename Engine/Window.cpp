242685595f274c43573e46c90d2cba27e5a2bf16
Sergey Timoshkin <faraser@yandex-team.ru>
2018-02-08 01:53:27 +0300

    Added physics engine and some falling boxes to NinjaPlatformer

--- Engine/Window.cpp
+++ Engine/Window.cpp
@@ -15,6 +15,8 @@ Window::~Window() {
 
 int Window::create(const std::string& windowName, int screenWidth, int screenHeight, unsigned int currentFlags) {
     Uint32 flags = SDL_WINDOW_OPENGL;
+    _screenWidth = screenWidth;
+    _screenHeight = screenHeight;
 
     if (currentFlags & INVISIBLE) {
         flags |= SDL_WINDOW_HIDDEN;
--- NinjaPlatformer/GameplayScreen.h
+++ NinjaPlatformer/GameplayScreen.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #include <Engine/IGameScreen.h>
+#include <Engine/SpriteBatch.h>
+#include <Engine/GLSLprogram.h>
+#include <Engine/Camera2D.h>
+#include <Engine/GLTexture.h>
+#include <Engine/Window.h>
+
+#include <Box2D/Box2D.h>
+#include "Box.h"
+#include <vector>
 
 class GameplayScreen : public Engine::IGameScreen {
 
 public:
-    GameplayScreen();
+    GameplayScreen(Engine::Window * window);
 
     ~GameplayScreen();
 
@@ -26,6 +35,15 @@ public:
     int getPreviousScreenIndex() const override;
 private:
     void checkInput();
+
+    Engine::GLSLprogram m_textureProgram;
+    Engine::SpriteBatch m_spriteBatch;
+    Engine::Camera2D m_camera;
+    Engine::GLTexture m_texture;
+    Engine::Window * m_window;
+
+    std::vector<Box> m_boxes;
+    std::unique_ptr<b2World> m_world;
 };
 
 
