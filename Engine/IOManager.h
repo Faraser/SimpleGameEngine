2163c4c9d3856a513caa1a743f851d5d67fe3898
Sergey Timoshkin <faraser@yandex-team.ru>
2018-02-10 18:35:21 +0300

    Refactor IOManager

--- Engine/IOManager.h
+++ Engine/IOManager.h
@@ -2,11 +2,32 @@
 
 #include <vector>
 #include <string>
+#include <fstream>
 
 namespace Engine {
 class IOManager {
 public:
-    static bool readFileToBuffer(const std::string& filePath, std::vector<unsigned char>& buffer);
-    static bool readFileToBuffer(const std::string& filePath, std::string& buffer);
+    template <typename Buffer>
+    static bool readFileToBuffer(const std::string& filePath, Buffer& buffer) {
+
+        std::ifstream file(filePath, std::ios::binary);
+        if (file.fail()) {
+            perror(filePath.c_str());
+            return false;
+        }
+
+        file.seekg(0, std::ios::end);
+
+        int fileSize = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        // Reduce the file size by any header bytes that might be present
+        fileSize -= file.tellg();
+
+        buffer.resize(fileSize);
+        file.read((char*) &buffer[0], fileSize);
+        file.close();
+        return true;
+    };
 };
 }
